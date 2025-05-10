b0e522ad7573d8500377739e95fcb7651ffc3dbc
develoeprboy1990 <mr.elahi.ehsan@gmail.com>
2025-05-11 03:35:26 +0500

    revert db changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
+//use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        Schema::defaultStringLength(191);
+        //Schema::defaultStringLength(191);
     }
 }
